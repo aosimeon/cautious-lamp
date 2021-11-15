b7e8510bcbfca1a206ede4b8b0d2bb9da9078640
Amoo Olusola Simeon <aosimeon@localhost.localdomain>
2021-11-15 08:58:51 +0100

    Add corrected notifications processed image

--- web/app/routes.py
+++ web/app/routes.py
@@ -71,15 +71,16 @@ def notification():
             ## TODO: Refactor This logic into an Azure Function
             ## Code below will be replaced by a message queue
             #################################################
-            # attendees = Attendee.query.all()
+            attendees = Attendee.query.all()
 
-            # for attendee in attendees:
-            #     subject = '{}: {}'.format(attendee.first_name, notification.subject)
-            #     send_email(attendee.email, subject, notification.message)
+            for attendee in attendees:
+                subject = '{}: {}'.format(attendee.first_name, notification.subject)
+                send_email(attendee.email, subject, notification.message)
 
-            # notification.completed_date = datetime.utcnow()
-            # notification.status = 'Notified {} attendees'.format(len(attendees))
-            # db.session.commit()
+            notification.completed_date = datetime.utcnow()
+            notification.status = 'Notified {} attendees'.format(len(attendees))
+            db.session.commit()
+            
             # TODO Call servicebus queue_client to enqueue notification ID
             notification_id = notification.id
 
@@ -106,12 +107,12 @@ def notification():
 
 
 def send_email(email, subject, body):
-    if not app.config.get('SENDGRID_API_KEY'):
+    if not os.environ["SENDGRID_API_KEY"]:
         message = Mail(
             from_email=app.config.get('ADMIN_EMAIL_ADDRESS'),
             to_emails=email,
             subject=subject,
             plain_text_content=body)
 
-        sg = SendGridAPIClient(app.config.get('SENDGRID_API_KEY'))
+        sg = SendGridAPIClient(os.environ["SENDGRID_API_KEY"])
         sg.send(message)
